746cb0eb2b9166a1ae247a44bcd404aa14b763bc
Sergiy Svitlookyi <sergiy.svitlookyi@itera.no>
2017-11-09 08:09:19 +0200

    Progress

---
+++ src/components/search-bar.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {term: ''};
+  }
+  
+  render() {
+    return (
+      <div>
+        <input
+          type="text"
+          onChange={this.onInputChange}
+        />
+        Va
+      </div>
+      
+    )
+  }
+  
+  onInputChange = (event) => {
+    this.setState({term: event.target.value});
+    console.log(event.target.value);
+    console.log(this.state);
+    {'value of the input:' {this.state.term}
+  }
+}
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
+import SearchBar from './components/search-bar';
 
 const API_KEY = 'AIzaSyAjik3pVy2w9ehRBcToBxexMLC556PshY0';
 
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar/>
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector('.container'));
